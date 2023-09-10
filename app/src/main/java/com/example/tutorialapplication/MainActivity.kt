26eefeb8f212ff23eabe5ae7cee2316591236084
falah.zaidan <falah.zaidan@outlook.com>
2023-09-10 13:42:20 +0100

    initial commit

---
+++ app/src/main/java/com/example/tutorialapplication/MainActivity.kt
@@ -0,0 +1,150 @@
+package com.example.tutorialapplication
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import com.example.tutorialapplication.ui.theme.TutorialApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TutorialApplicationTheme {
+                //Alignment using a Column - items are stacked underneath each other, comment them in/out
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Top,
+                ) {
+                    TestText("Falah")
+                    ListExample()
+                    TestButton()
+                    ImageExample()
+                }
+
+                //Alignment using a Box - items stacked on top of each other, comment them in/out
+//                Box(
+//                    modifier = Modifier
+//                        .fillMaxSize(),
+//                    contentAlignment = Alignment.TopCenter
+//                ) {
+//                    TestText("Falah")
+//                    ListExample()
+//                    TestButton()
+//                    ImageExample()
+//                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TestText(name: String) {
+
+    //Basic Text
+    Text("Basic text")
+
+    //Text with Padding (and using the name parameter)
+    Text(
+        modifier = Modifier.padding(10.dp),
+        text = "My name is $name"
+    )
+
+    //Text stacked in a Column
+    Column {
+        Text(text = "Hello world")
+        Text(text = "Text that is underneath Hello World")
+    }
+
+    //Text in a Row
+    Row {
+        Text("First item, ")
+        Text("Second item")
+    }
+}
+
+@Composable
+fun ListExample() {
+
+    val listOfNames = listOf("Najey", "Falah", "Luke", "Carlo", "Toufik")
+
+    LazyColumn {
+        items(listOfNames) { item ->
+            Text(item)
+        }
+    }
+
+}
+
+@Composable
+fun TestButton() {
+    Button(
+        onClick = {
+            Log.d("MainActivityLog", "Button was clicked")
+        },
+        modifier = Modifier
+    ) {
+        Text("Test button")
+    }
+}
+
+@Composable
+fun ImageExample() {
+
+    //PNG dragged into project (in res/drawable folder)
+    Image(
+        painterResource(id = R.drawable.tree),
+        contentDescription = "Tree image"
+    )
+
+    //Vector asset created using the Clip art library (right click drawable folder -> New -> Vector Asset)
+    Image(
+        painterResource(id = R.drawable.ic_android_black_24dp),
+        contentDescription = "Android symbol"
+    )
+
+    //Image from a URL using the Coil library for Android
+    val image =
+        rememberImagePainter(data = "https://media.macphun.com/img/uploads/macphun/blog/2063/_1.jpeg?q=75&w=1710&h=906&resize=cover")
+
+    Image(
+        painter = image,
+        contentDescription = "a Landscape",
+        modifier = Modifier
+            .size(100.dp)
+            .padding(16.dp)
+            .clip(CircleShape),
+        contentScale = ContentScale.Crop,
+        alignment = Alignment.Center
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainActivityPreview() {
+    TutorialApplicationTheme {
+        TestText("Falah")
+    }
+}
